5b7a3d549a5fceec71501478133bfb4d4c7b1ebb
farras <mfarras196@gmail.com>
2024-05-17 04:31:20 +0700

    change year footer

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -259,11 +259,8 @@
   }
 })(jQuery);
 
-// years footer
+// Year footer
 $(document).ready(function() {
-  // Get the current year
   const currentYear = new Date().getFullYear();
-
-  // Set the text content of the element with id 'year' to the current year
   $('#year').text(currentYear);
 });
